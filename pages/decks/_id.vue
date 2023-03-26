6957925cade17e8e3d54aaa72516da69cc821f49
nhuanh9 <chopperlouis@gmail.com>
2023-03-27 00:15:14 +0700

    done edit

--- pages/decks/_id.vue
+++ pages/decks/_id.vue
@@ -1,3 +1,46 @@
 <template>
-  <h1>Detail Deck {{$route.params.id}}</h1>
+  <div>
+    <form @submit.prevent="updateProduct">
+      <input v-model="name" placeholder="Product name"/>
+      <input v-model="price" placeholder="Product price"/>
+      <input v-model="description" placeholder="Description price"/>
+      <button type="submit">Update product</button>
+    </form>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+      price: "",
+      description: ""
+    };
+  },
+
+  async mounted() {
+    const productId = this.$route.params.id;
+    const response = await this.$axios.get(`http://localhost:8005/api/products/${productId}`);
+    const {name, price, description} = response.data;
+    this.name = name;
+    this.price = price;
+    this.description = description;
+  },
+
+  methods: {
+    async updateProduct() {
+      const productId = this.$route.params.id;
+      const response = await this.$axios.put(`http://localhost:8005/api/products/${productId}`, {
+        name: this.name,
+        price: this.price,
+        description: this.description,
+        created_at: "2023-03-23T22:43:08.000000Z",
+        updated_at: "2023-03-23T22:43:09.000000Z"
+      });
+
+      this.$router.push('/')
+    },
+  },
+};
+</script>
