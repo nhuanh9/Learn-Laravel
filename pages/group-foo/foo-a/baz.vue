4ef9a39f79b40549f7fb430f1d1adf7e932983ac
nhuanh9 <chopperlouis@gmail.com>
2023-03-27 14:44:57 +0700

    done test route

---
+++ pages/group-foo/foo-a/baz.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Bazzz</h1>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,8 @@
   <div>
     <h1>Hello</h1>
     <div v-for="item in decks">
-      {{item.name}}
+      {{ item.name }}
+      <button @click="delProduct(item.id)">Delete</button>
     </div>
   </div>
 </template>
@@ -14,13 +15,17 @@ export default {
       decks: []
     }
   },
-  async asyncData({ $axios }) {
-    let { data } = await $axios.get("");
-    return { decks: data };
+  async asyncData({$axios}) {
+    let {data} = await $axios.get("");
+    return {decks: data};
   },
   methods: {
     async showDeck() {
       let res = await this.$store.dispatch("getDecks");
+    },
+    async delProduct(id) {
+      await this.$axios.delete("/" + id);
+      this.$router.push('/')
     }
   },
   name: 'IndexPage',
